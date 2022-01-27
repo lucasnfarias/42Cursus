fb48d2b953686c391b900bab308bf07e41ee3ccf
Lucas Farias <lucasnfarias01@gmail.com>
2022-01-27 01:48:28 +0100

    (feat) add ex00 files

---
+++ cpp/module00/ex00/megaphone.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 20:14:48 by lniehues          #+#    #+#             */
+/*   Updated: 2022/01/27 01:45:34 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	ft_toupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (c - 32);
+	return (c);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	count;
+
+	count = 0;
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
+
+void print_str_upperized(char *str) {
+	int len = ft_strlen(str);
+	for (int i = 0; i < len; i++) {
+		std::cout << (char)ft_toupper(str[i]);
+	}
+	return;
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			print_str_upperized(argv[i]);
+		}
+		std::cout << std::endl;
+	}
+
+	return (0);
+}
