cca01ab281031c46917e894f3edf7f2739c76855
Lucas Farias <lucasnfarias01@gmail.com>
2022-03-15 22:02:19 -0300

    (feat/cpp/module01) create module01 folder and add complete ex00

---
+++ cpp/module01/ex00/newZombie.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 21:32:13 by lniehues          #+#    #+#             */
+/*   Updated: 2022/03/15 21:49:29 by lniehues         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* newZombie( std::string name )
+{
+  Zombie* newZombie = new Zombie(name);
+
+  return newZombie;
+}
