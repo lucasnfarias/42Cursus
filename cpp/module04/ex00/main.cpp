e22b26ed095e4c52da57ab32cefdcac76cd413a6
Lucas Farias <lucasnfarias01@gmail.com>
2022-04-26 16:41:28 -0300

    (refactor/cpp/module04) add some logs to main tests on ex00

--- cpp/module04/ex00/main.cpp
+++ cpp/module04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 20:05:39 by lniehues          #+#    #+#             */
-/*   Updated: 2022/04/25 16:57:47 by lniehues         ###   ########.fr       */
+/*   Updated: 2022/04/26 16:39:25 by lniehues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ const Animal* dog = new Dog();
 const Animal* cat = new Cat();
 
 std::cout << "# TYPES:" << std::endl;
-std::cout << dog->getType() << std::endl;
-std::cout << cat->getType() << std::endl;
-std::cout << unicorn->getType() << std::endl;
+std::cout << "DOG: " << dog->getType() << std::endl;
+std::cout << "CAT: " << cat->getType() << std::endl;
+std::cout << "ANIMAL: " << unicorn->getType() << std::endl;
 
 std::cout << "# SOUNDS:" << std::endl;
-cat->makeSound();
 dog->makeSound();
+cat->makeSound();
 unicorn->makeSound();
 
 delete cat;
@@ -43,8 +43,8 @@ const WrongAnimal* wrongUnicorn = new WrongAnimal();
 const WrongAnimal* wrongCat = new WrongCat();
 
 std::cout << "# TYPES:" << std::endl;
-std::cout << wrongCat->getType() << std::endl;
-std::cout << wrongUnicorn->getType() << std::endl;
+std::cout << "CAT: " << wrongCat->getType() << std::endl;
+std::cout << "ANIMAL: " << wrongUnicorn->getType() << std::endl;
 
 std::cout << "# SOUNDS:" << std::endl;
 wrongCat->makeSound();
