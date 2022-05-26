f154a48e5736f55bcc508522635e2d9870989ccb
Lucas Farias <lucasnfarias01@gmail.com>
2022-05-25 21:32:01 -0300

    (feat/cpp/module06) add base files to ex02

---
+++ cpp/module06/ex02/IdentifyRealType.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IdentifyRealType.cpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 21:28:07 by lniehues          #+#    #+#             */
+/*   Updated: 2022/05/25 21:29:30 by lniehues         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "IdentifyRealType.hpp"
+
+Base * generate(void)
+{}
+
+void identify(Base* p)
+{}
+
+void identify(Base& p)
+{}
---
+++ cpp/module06/ex02/IdentifyRealType.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IdentifyRealType.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 21:25:13 by lniehues          #+#    #+#             */
+/*   Updated: 2022/05/25 21:29:19 by lniehues         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IDENTIFYREALTYPE_HPP
+# define IDENTIFYREALTYPE_HPP
+
+class Base {
+  virtual ~Base();
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
+
+#endif
