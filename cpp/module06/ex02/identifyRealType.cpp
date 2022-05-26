ddec52306687c025b35e6242ff594949d6329ec1
Lucas Farias <lucasnfarias01@gmail.com>
2022-05-25 21:34:02 -0300

    (feat/cpp/module06) renaming files and executable on ex02

--- cpp/module06/ex02/identifyRealType.cpp
+++ cpp/module06/ex02/identifyRealType.cpp
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   IdentifyRealType.cpp                               :+:      :+:    :+:   */
+/*   identifyRealType.cpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 21:28:07 by lniehues          #+#    #+#             */
-/*   Updated: 2022/05/25 21:29:30 by lniehues         ###   ########.fr       */
+/*   Updated: 2022/05/25 21:33:48 by lniehues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "IdentifyRealType.hpp"
+#include "identifyRealType.hpp"
 
 Base * generate(void)
 {}
