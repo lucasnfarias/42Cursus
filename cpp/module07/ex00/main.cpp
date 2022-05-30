089b67b9df5adf1555cf4a95c79ea6ce435de084
Lucas Farias <lucasnfarias01@gmail.com>
2022-05-30 03:14:38 +0200

    (feat/cpp/module07) add basic ex00 files

---
+++ cpp/module07/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 03:10:43 by coder             #+#    #+#             */
+/*   Updated: 2022/05/30 03:11:32 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main( void ) {
+  int a = 2;
+  int b = 3;
+
+  ::swap( a, b );
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+  std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+  std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+  return 0;
+}
---
+++ cpp/module07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 03:12:17 by coder             #+#    #+#             */
+/*   Updated: 2022/05/30 03:13:58 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	WHATEVER_HPP
+# define WHATEVER_HPP
+
+template<typename T>
+T	swap(T a, Tb);
+
+template<typename T>
+T	min(T a, Tb);
+
+template<typename T>
+T	max(T a, Tb);
+
+#endif
