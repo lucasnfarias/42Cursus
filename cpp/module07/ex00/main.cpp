38c3f752181733c8cbbdd63ce5e2211e1f457f01
Lucas Farias <lucasnfarias01@gmail.com>
2022-05-31 00:18:26 +0200

    (feat/cpp/module07) add complete ex00

--- cpp/module07/ex00/main.cpp
+++ cpp/module07/ex00/main.cpp
@@ -6,11 +6,16 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 03:10:43 by coder             #+#    #+#             */
-/*   Updated: 2022/05/30 03:11:32 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/30 23:53:33 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "whatever.hpp"
+
 int main( void ) {
+  std::cout << "\n# TEST - DIFFERENT INTS - a: 2, b: 3\n" << std::endl;
+  
   int a = 2;
   int b = 3;
 
@@ -19,6 +24,8 @@ int main( void ) {
   std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
   std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
+  std::cout << "\n# TEST - DIFFERENT STRINGS - c: chaine1, d: chaine2\n" << std::endl;
+
   std::string c = "chaine1";
   std::string d = "chaine2";
 
@@ -26,6 +33,16 @@ int main( void ) {
   std::cout << "c = " << c << ", d = " << d << std::endl;
   std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
   std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+  std::cout << "\n# TEST - EQUAL FLOAT s- x: 42, y: 42\n" << std::endl;
+
+  float x = 42.0;
+  float y = 42.0;
+
+  ::swap( x, y );
+  std::cout << "x = " << x << ", y = " << y << std::endl;
+  std::cout << "min( x, y ) = " << ::min( x, y ) << std::endl;
+  std::cout << "max( x, y ) = " << ::max( x, y ) << std::endl;
 
   return 0;
 }
--- cpp/module07/ex00/whatever.hpp
+++ cpp/module07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 03:12:17 by coder             #+#    #+#             */
-/*   Updated: 2022/05/30 03:13:58 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/30 23:55:53 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 # define WHATEVER_HPP
 
 template<typename T>
-T	swap(T a, Tb);
+void	swap(T &a, T &b)
+{
+	T	temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
 
 template<typename T>
-T	min(T a, Tb);
+T	const &min(T const &a, T const &b)
+{
+	return (a < b ? a : b);
+}
 
 template<typename T>
-T	max(T a, Tb);
+T	const &max(T const &a, T const &b)
+{
+	return (a > b ? a : b);
+}
 
 #endif
