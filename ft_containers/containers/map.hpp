eb3b5dd5d784eed1ba7f5d386e78ff1c83e8e8b7
lucasnfarias <lucasnfarias01@gmail.com>
2022-06-20 21:57:31 -0300

    (feat/ft_containers) add base files

---
+++ ft_containers/containers/map.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 21:27:34 by lniehues          #+#    #+#             */
+/*   Updated: 2022/06/20 21:32:33 by lniehues         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MAP_HPP
+# define FT_MAP_HPP
+# include <iostream>
+
+namespace ft {
+  // template < class Key,                                     // map::key_type
+  //          class T,                                       // map::mapped_type
+  //          class Compare = less<Key>,                     // map::key_compare
+  //          class Alloc = allocator<pair<const Key,T> >    // map::allocator_type
+  //          > class map;
+}
+
+#endif
---
+++ ft_containers/containers/vector.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 21:27:34 by lniehues          #+#    #+#             */
+/*   Updated: 2022/06/20 21:30:51 by lniehues         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_VECTOR_HPP
+# define FT_VECTOR_HPP
+# include <iostream>
+
+namespace ft {
+  template < class T, class Alloc = std::allocator<T> >
+  class vector;
+}
+
+#endif
