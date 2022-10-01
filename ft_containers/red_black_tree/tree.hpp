22c2f8e30d0384fb3975e1e223ff4de89f442755
lucasnfarias <lucasnfarias01@gmail.com>
2022-09-30 21:42:48 -0300

    (fix/ft_containers) remove unnecessary comment

--- ft_containers/red_black_tree/tree.hpp
+++ ft_containers/red_black_tree/tree.hpp
@@ -6,7 +6,7 @@
 /*   By: lniehues <lniehues@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 20:37:06 by lniehues          #+#    #+#             */
-/*   Updated: 2022/09/30 21:11:07 by lniehues         ###   ########.fr       */
+/*   Updated: 2022/09/30 21:41:37 by lniehues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -327,8 +327,6 @@ private:
   {
     return upper_bound(value.first)-- == position;
   }
-
-    // CHECK IF DEEP COPY WORKS or look on the internet
 
   node_pointer _copyTree(
     const node_pointer &node,
